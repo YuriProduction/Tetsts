de3423ffe00bedca64c584e4532dca14e7817acf
Jurex <jurexda@yandex.ru>
2024-04-27 18:10:48 +0500

    NoteLogic tests done!

---
+++ src/test/java/example/note/NoteLogicTest.java
@@ -0,0 +1,62 @@
+package example.note;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NoteLogicTest
+{
+    /**
+     * Тестируем команду /add
+     */
+    @Test
+    public void testAddCommand()
+    {
+        NoteLogic noteLogic = new NoteLogic();
+        final String userMessage = "/add Сделать матанализ к среде";
+        final String answer = noteLogic.handleMessage(userMessage);
+        assertEquals(answer, "Note added!");
+        final String notes = noteLogic.handleMessage("/notes");
+        assertEquals(notes, "Your notes:\nСделать матанализ к среде");
+    }
+
+    /**
+     * Тестируем команду /notes
+     */
+    @Test
+    public void testNotesCommand()
+    {
+        NoteLogic noteLogic = new NoteLogic();
+        noteLogic.handleMessage("/add NewNote");
+        assertEquals(noteLogic.handleMessage("/notes"), "Your notes:\nNewNote");
+    }
+
+    /**
+     * Тестируем команду /edit
+     */
+    @Test
+    public void testEditCommand()
+    {
+        NoteLogic noteLogic = new NoteLogic();
+        noteLogic.handleMessage("/add Note");
+        noteLogic.handleMessage("/edit Note Note1");
+        assertEquals(noteLogic.handleMessage("/notes"),
+                "Your notes:\n" +
+                        "Note1");
+    }
+
+    /**
+     * Тестируем команду /del
+     */
+    @Test
+    public void testDelCommand()
+    {
+        NoteLogic noteLogic = new NoteLogic();
+        noteLogic.handleMessage("/add MyNote");
+        final String answer = noteLogic.handleMessage("/del MyNOTE");
+        assertNotEquals(answer, "Note deleted!");
+        assertEquals(noteLogic.handleMessage("/notes"),
+                "Your notes:");
+    }
+}
